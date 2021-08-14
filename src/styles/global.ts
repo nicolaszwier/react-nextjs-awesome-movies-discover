820752eb78adebaed656142e0d8655f347e0b500
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2021-08-14 17:03:59 -0300

    new variables css

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,17 +7,24 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  html {
-    font-size: 62.5%;
-  }
-
   html, body, #__next {
     height: 100%;
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+    --background: rgb(255, 255, 255);
+    --backgroundContrast: #F9F9F9;
+    --primary: #5E1AB7;
+    --primaryLight: #8758C4;
+    --secondary: #ECC500;
+    --textColorOnBackground: rgba(0, 0, 0, 0.85);
+    --secondaryTextColor: rgba(0,0,0,0.65);
+    --border: rgba(0,0,0,0.10)
   }
+
+  
 `
 
 export default GlobalStyles
