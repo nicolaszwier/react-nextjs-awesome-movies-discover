1493be681b1d38a7f32419763e9289ba7f6b7637
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2021-08-15 20:54:52 -0300

    changes on filters component

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,14 +1,25 @@
 import { apiKey, apiUrl } from 'config/config'
 import { Genre } from 'models/Genre'
+import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import * as S from './styles'
 
 const Filters = () => {
+  const router = useRouter()
   const [genres, setGenres] = useState<Genre[]>([])
+  const [selectedGenres, setSelectedGenres] = useState<number[]>([])
 
   useEffect(() => {
     fetchGenresList()
   }, [])
+
+  useEffect(() => {
+    if (router.query.genres && router.isReady) {
+      const selectedGenresFromQuery = router.query.genres.toString().split(',')
+      const genresArray = selectedGenresFromQuery.map((el) => Number(el))
+      setSelectedGenres(genresArray)
+    }
+  }, [router.query.genres, router.isReady])
 
   const fetchGenresList = async () => {
     try {
@@ -32,15 +43,61 @@ const Filters = () => {
     }
   }
 
+  const unselectGenre = (genreId: number) => {
+    const genresCopy = [...selectedGenres]
+    const index = selectedGenres.findIndex((el) => el === genreId)
+    genresCopy.splice(index, 1)
+    setSelectedGenres(genresCopy)
+  }
+
+  const toggleGenre = (genreId: number) => {
+    if (isGenreSelected(genreId)) {
+      unselectGenre(genreId)
+    } else {
+      setSelectedGenres([...selectedGenres, genreId])
+    }
+  }
+
+  const isGenreSelected = (id: number) => {
+    const index = selectedGenres.findIndex((el) => el === id)
+    return index >= 0
+  }
+
+  const applyFilter = () => {
+    updateQueryParams(selectedGenres.join(','))
+  }
+
+  const updateQueryParams = (genres?: string) => {
+    router.push(
+      { pathname: '/', query: { ...router.query, genres: genres } },
+      undefined,
+      {
+        shallow: true,
+        scroll: true
+      }
+    )
+  }
+
   return (
     <S.Wrapper>
       <S.FiltersGroupWrapper>
         <S.FiltersGroupTitle>Genres</S.FiltersGroupTitle>
         <S.FiltersGroupContent>
-          {genres.map((el: Genre) => (
-            <S.Chip key={el.id}>{el.name}</S.Chip>
+          {genres.map((genre: Genre) => (
+            <S.Chip
+              className={isGenreSelected(genre.id) ? 'selected' : ''}
+              onClick={() => {
+                toggleGenre(genre.id)
+              }}
+              key={genre.id}
+            >
+              {genre.name}
+            </S.Chip>
           ))}
         </S.FiltersGroupContent>
+        <S.ApplyFilterButton onClick={applyFilter}>
+          Apply filter
+        </S.ApplyFilterButton>
       </S.FiltersGroupWrapper>
     </S.Wrapper>
   )
